644be8b90b9f608c259bef486258dbfe9d9e9b80
eleanors <441656497@qq.com>
2016-03-28 00:01:09 +0800

    add.gulp

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var gulp = require('gulp');
+var merge = require('merge-stream');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+
+var scriptsPath = 'js/';
+
+function getFolders(dir) {
+        return fs.readdirSync(dir).filter(function(file) {
+                return fs.statSync(path.join(dir, file)).isDirectory();
+        });
+}
+
+gulp.task('default',function() {
+        var folders = getFolders(scriptsPath);
+
+        var tasks = folders.map(function(folder) {
+                // 拼接成 foldername.js
+                // 写入输出
+                // 压缩
+                // 重命名为 folder.min.js
+                // 再一次写入输出
+                return gulp.src(path.join(scriptsPath, folder, '/*.js'))
+				       .pipe(concat(folder + '.js'))
+					   .pipe(gulp.dest(scriptsPath))
+					   .pipe(uglify())
+					   .pipe(rename(folder + '.min.js'))
+					   .pipe(gulp.dest(scriptsPath));
+        });
+
+        return merge(tasks);
+});
